e4d978b2fa8deaadcea77d54264ee3bd9119631c
iifast2 <60607576+iifast2@users.noreply.github.com>
2023-02-16 08:07:02 +0100

    Commit 2.1
    :white_check_mark:
    
    Commit 2.1  :
    
    * AlertDialog doesn't show up in web !
    
    * Reformat code

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,3 @@
-//import 'dart:js';
-//import 'package:path/path.dart';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:modernlogintute/components/my_button.dart';
@@ -22,81 +19,70 @@ class _LoginPageState extends State<LoginPage> {
 
   // sign user in method
   void signUserIn() async {
-
     // show a loading circle while the user logs in ~ because that will take a lil bit of time
     // https://stackoverflow.com/a/63993275/10216101
 
-   showDialog(context: context, builder: (context){
-     return const Center(
-       child: CircularProgressIndicator(),
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
 
+    // Sign in
 
-
-     );
-
-   },);
-
-     // Sign in
-
-   try{
-     await FirebaseAuth.instance.signInWithEmailAndPassword(
-       email: emailController.text,
-       password: passwordController.text,
-     );
-
-   } on FirebaseAuthException catch (e) {
-
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+    } on FirebaseAuthException catch (e) {
       // this will pop the circle then shows the errors !
-     Navigator.pop(context);
+      Navigator.pop(context);
 
-     // WRONG EMAIL
-     if(e.code == 'user-not-found'){
-         // show error to user
-       WrongEmailMessage();
-       print('No user found for that email !!!!');
-
-
-       // WRONG PASSWORD
-     }else if(e.code == 'wrong-password'){
-
-
+      // WRONG EMAIL
+      if (e.code == 'user-not-found') {
         // show error to user
-       WrongPasswordMessage();
-       // this will print in the console !!
-       print('Wrong password !!!!');
-     }
-   }
+        WrongEmailMessage();
+        print('No user found for that email !!!!');
 
+        // WRONG PASSWORD
+      } else if (e.code == 'wrong-password') {
+        // show error to user
+        WrongPasswordMessage();
+        // this will print in the console !!
+        print('Wrong password !!!!');
+      }
+    }
 
     // pop the loading circle ~ after it loads , this will make it go away
     Navigator.pop(context);
   }
 
-
-
-
-
-  void WrongEmailMessage(){
-    showDialog(context: context, builder: (context){
-      return const AlertDialog(
-        title: Text('Incorrect Email'),
-       );
+  void WrongEmailMessage() {
+    // this doesn't work on web !!? it doesn't show dialog
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          title: Text('Incorrect Email'),
+        );
       },
     );
   }
 
-  void WrongPasswordMessage(){
-    showDialog(context: context, builder: (context){
-      return const AlertDialog(
-        title: Text('Incorrect Password'),
-      );
-    },
+  void WrongPasswordMessage() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          title: Text('Incorrect Password'),
+        );
+      },
     );
-
   }
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -106,169 +92,164 @@ class _LoginPageState extends State<LoginPage> {
         child: SingleChildScrollView(
           child: Center(
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                const SizedBox(height: 50),
-            // stack the logo image over the rest of the content
-            Stack(
-              children: [
-              // logo
-              Positioned(
-
-              right: 5,
-              child: Image.asset(
-                'lib/images/theteamlogo.png',
-                height: 40,
-              ),
-            ),
-            // the rest of the content
-
-                Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const SizedBox(height: 50),
-
-                const Icon(
-                  Icons.lock,
-                  size: 100,
-                ),
-                const SizedBox(height: 50),
-                // welcome back, you've been missed!
-                Text(
-                  'Welcome back you\'ve been missed!',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 25),
-                // email textfield
-                MyTextField(
-                  controller: emailController,
-                  hintText: 'Email: admin@gmail.com',
-                  obscureText: false,
-                ),
-                const SizedBox(height: 10),
-                // password textfield
-                MyTextField(
-                  controller: passwordController,
-                  hintText: 'Password: admin123',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 10),
-                // forgot password?
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Forgot Password?',
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 25),
-                // sign in button
-                MyButton(
-                  onTap: signUserIn,
-                ),
-                const SizedBox(height: 50),
-                // or continue with
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Text(
-                          'Or continue with',
-                          style: TextStyle(color: Colors.grey[700]),
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 50),
-                // google + apple sign in buttons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    // google button
-                    SquareTile(imagePath: 'lib/images/google.png'),
-
-
-                    SizedBox(width: 15),
-
-                    // apple button
-                    SquareTile(imagePath: 'lib/images/apple.png'),
-
-                    /////////////////////////// I added these buttons //////////////////////////////
-
-                    SizedBox(width: 15),
-                    // facebook button
-                    SquareTile(imagePath: 'lib/images/facebook.png'),
-
-                    //////////////////////////////////////////////////////////////////////////////////////////
-                  ],
-                ),
-
-                const SizedBox(height: 30),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-
-                    SizedBox(width: 15),
-                    // Linkedin button
-                    SquareTile(imagePath: 'lib/images/linkedin.png'),
-
-                    SizedBox(width: 15),
-                    // anonymous button
-                    SquareTile(imagePath: 'lib/images/anonymous.png'),
-
-                  ],
-                ),
-
-                const SizedBox(height: 30),
-
-                // not a member? register now
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                // stack the logo image over the rest of the content
+                Stack(
                   children: [
-                    Text(
-                      'Not a member?',
-                      style: TextStyle(color: Colors.grey[700]),
-                    ),
-                    const SizedBox(width: 4),
-                    const Text(
-                      'Register now',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
+                    // logo
+                    Positioned(
+                      right: 5,
+                      child: Image.asset(
+                        'lib/images/theteamlogo.png',
+                        height: 40,
                       ),
+                    ),
+                    // the rest of the content
+
+                    Column(
+                      children: [
+                        const SizedBox(height: 50),
+
+                        const Icon(
+                          Icons.lock,
+                          size: 100,
+                        ),
+                        const SizedBox(height: 50),
+                        // welcome back, you've been missed!
+                        Text(
+                          'Welcome back you\'ve been missed!',
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                        // email textfield
+                        MyTextField(
+                          controller: emailController,
+                          hintText: 'Email: admin@gmail.com',
+                          obscureText: false,
+                        ),
+                        const SizedBox(height: 10),
+                        // password textfield
+                        MyTextField(
+                          controller: passwordController,
+                          hintText: 'Password: admin123',
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 10),
+                        // forgot password?
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                'Forgot Password?',
+                                style: TextStyle(color: Colors.grey[600]),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                        // sign in button
+                        MyButton(
+                          onTap: signUserIn,
+                        ),
+                        const SizedBox(height: 50),
+                        // or continue with
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Divider(
+                                  thickness: 0.5,
+                                  color: Colors.grey[400],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10.0),
+                                child: Text(
+                                  'Or continue with',
+                                  style: TextStyle(color: Colors.grey[700]),
+                                ),
+                              ),
+                              Expanded(
+                                child: Divider(
+                                  thickness: 0.5,
+                                  color: Colors.grey[400],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        // google + apple sign in buttons
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            // google button
+                            SquareTile(imagePath: 'lib/images/google.png'),
+
+                            SizedBox(width: 15),
+
+                            // apple button
+                            SquareTile(imagePath: 'lib/images/apple.png'),
+
+                            SizedBox(width: 15),
+                            // facebook button
+                            SquareTile(imagePath: 'lib/images/facebook.png'),
+                          ],
+                        ),
+
+                        const SizedBox(height: 30),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            SizedBox(width: 15),
+                            // Linkedin button
+                            SquareTile(imagePath: 'lib/images/linkedin.png'),
+
+                            SizedBox(width: 15),
+                            // anonymous button
+                            SquareTile(imagePath: 'lib/images/anonymous.png'),
+                          ],
+                        ),
+
+                        const SizedBox(height: 30),
+
+                        // not a member? register now
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Not a member?',
+                              style: TextStyle(color: Colors.grey[700]),
+                            ),
+                            const SizedBox(width: 4),
+                            const Text(
+                              'Register now',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(height: 30),
+                      ],
                     ),
                   ],
                 ),
-
-                const SizedBox(height: 30),
               ],
             ),
-          ],),
-        ],),
-      ),
+          ),
         ),
       ),
     );
